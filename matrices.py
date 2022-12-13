1becc0e33e18094d5eb2dac731c8d00bbf0f5a30
stelin41 <77170009+stelin41@users.noreply.github.com>
2022-12-13 15:11:00 +0000

    fix2

--- matrices.py
+++ matrices.py
@@ -109,7 +109,7 @@ class Matriz:
             return self.contenido_matriz[elemento-1]
 
         except ValueError:
-            raise "La fila y la columna especificada debe ser un número entero mayor que 0"
+            raise Exception("La fila y la columna especificada debe ser un número entero mayor que 0")
 
 
 
@@ -124,7 +124,7 @@ class Matriz:
             self.contenido_matriz[elemento-1] = valor
 
         except ValueError:
-            return "La fila y la columna especificada debe ser un número entero mayor que 0"
+            return Exception("La fila y la columna especificada debe ser un número entero mayor que 0")
 
 
     def pide_matriz(self):
