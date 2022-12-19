bdbe5a2b1cb69949a6caaa80621d167fe181bd83
stelin41 <stelin41.io@gmail.com>
2022-12-20 00:55:10 +0100

    wow

--- menu.py
+++ menu.py
@@ -40,11 +40,60 @@ def carga_estado():
 
         except:
             print("El archivo", nombre_archivo, "no esixte o no es válido. Por favor, inténtalo de nuevo.")
-
+    print("Contenido cargado con éxito.")
     return lista_matrices
+
+
+def guarda_estado():
+    nombre_archivo = input("Introduce el nombre completo del archivo: ")
+
+    print("Contenido guardado con éxito.")
+
+
+def listar_matrices(matrices):
+    for k in matrices.keys():
+        print()
+        print(f"{k}:")
+        matrices[k].imprime()
+
+
+def nuevo_nombre(ultimo_nombre):
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def  _incrementa(letra):
+        alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if letra == "Z":
+            return "A"
+        else:
+            indice = alfabeto.find(letra)
+            return alfabeto[indice+1]
+
+    if ultimo_nombre == "":
+        return "A"
+    else:
+        nuevo_nombre = ""
+
+        # Si esto ocurre entonces es necesario crear una nueva letra
+        if len(set(ultimo_nombre)) == 1 and list(set(ultimo_nombre))[0] == "Z":
+            nuevo_nombre = "A"*(len(ultimo_nombre)+1)
+
+        else:
+            ultimo_indice = 0
+            for i in range(len(ultimo_nombre)-1, -1, -1):
+                ultimo_indice = i
+                nueva_letra = _incrementa(ultimo_nombre[i])
+                nuevo_nombre = nueva_letra + nuevo_nombre
+
+                # Si esto no se cumple entonces "se lleva una", por lo que deve seguir incrementando las siguientes letras
+                if nueva_letra != "A":
+                    break
             
+            nuevo_nombre = ultimo_nombre[:i] + nuevo_nombre
+
+        return nuevo_nombre
 
 def pide_operacion():
+    print()
     print("Lista de posibles acciones:")
     print("(1) Definir matriz")
     print("(2) Listar matrices creadas")
@@ -64,24 +113,28 @@ def pide_operacion():
             
         except:
             print("Debes introducir un número del 1 al 6 (incluidos). Por favor, inténtalo de nuevo.")
+            print()
 
 def main():
-    matrices = []
+    matrices = {}
+    nombre_inventado = ""
 
     print("Herramienta para realizar cálculos con matrices.")
     print("")
     print("¿Deseas cargar el estado previo de las matrices?")
     if si_o_no(False):
-        matrices = carga_estado()
+        matrices_cargadas = carga_estado()
+        for matriz in matrices_cargadas:
+            nombre_inventado = nuevo_nombre(nombre_inventado)
+            matrices[nombre_inventado] = matriz
 
-    salir = False
-    while not salir:
+    while True:
         operacion = pide_operacion()
 
         if operacion == 1:
             print("a")
         elif operacion == 2:
-            print("b")
+            listar_matrices(matrices)
         elif operacion == 3:
             print("c")
         elif operacion == 4:
